21000a3db24b97a42117c51b53fdc9c76b23784f
writh <victoria.thursday@gmail.com>
2018-08-09 18:34:39 -0600

    started setting up server and controller

---
+++ ipto/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const controller = require('./server/controller')
+const massive = require('massive');
+const helmet = require('helmet')
+const cors = require('cors');
+const session = require('express-session')
+require('dotenv').config();
+
+const app = express()
+app.use(helmet())
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  }))
+app.use(bodyParser.json())
+app.use(cors())
+massive(process.env.DB_CONNECT)
+.then(db=>{
+    app.set("db", db)
+    console.log("db Connected")
+})
+.catch(err => console.log(err))
+
+app.post('/api/createreview', controller.createReview)
+app.post('/api/createlocation', controller.createLocation)
+app.patch('/api/editreview', controller.editReview)
+app.delete('/api/deletereview', controller.deleteReview)
+app.get('/api/getlocation/:id', controller.getLocation)
+
+const port = 3005
+
+app.listen(port, () => {
+    console.log(`Server listening at localhost:${port}`);
+});
---
+++ ipto/server/controller.js
@@ -0,0 +1,110 @@
+module.exports = {
+       
+    create: (req, res, next)=>{
+        const dbInstance = req.app.get('db');
+        const {username, password, profilePic} = req.body
+
+        dbInstance.add_user([username, password, profilePic]).then(()=> res.status(200).send({message: "added item to database"}))
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Error!' });
+            console.log(err);
+         })
+    },    
+
+    loginUser: (req, res, next)=>{
+        const { username, password } = req.body
+        const dbInstance = req.app.get('db')
+
+        dbInstance.login_user([username, password])
+        .then(user=> {
+            req.session.user_id= user[0].id
+            res.status(200).send(user)
+        })
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+    },
+
+    getPosts: (req, res, next)=>{
+        const dbInstance = req.app.get('db')
+        const {search, user_posts} = req.query
+        const newSearch = '%'+search+'%'
+        const id = req.session.user_id
+        
+        if(search && user_posts){
+        dbInstance.get_posts_filtered([newSearch, id])
+        .then(posts=> res.status(200).send(posts))
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+
+
+    }else if(search){
+        dbInstance.get_posts_search([newSearch])
+        .then(posts=> res.status(200).send(posts))
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+
+    }else if(user_posts){
+        dbInstance.get_posts_user([id])
+        .then(posts=> res.status(200).send(posts))
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+    }
+    else{
+        dbInstance.get_posts([])
+        .then(posts=> res.status(200).send(posts))
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+    }
+},
+    getSinglePost: (req, res, next)=>{
+        const dbInstance = req.app.get('db')
+        const {id} = req.params
+        console.log('id of req.params', req.params)
+        dbInstance.get_single_post([id]).then(post=>{
+            console.log('post', post)
+            res.status(200).send(post)
+        })
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+        
+    },
+    newPost: (req, res, next)=>{
+        const dbInstance = req.app.get('db')
+        const {title, image_url, content } = req.body
+        const user_id = req.session.user_id
+        dbInstance.add_post([title, image_url, content, user_id])
+        .then(()=> { res.status(200).send({message: 'item added to database'})})
+        .catch((err) => {
+         res.status(500).send({ errorMessage: 'Something went wrong!' });
+         console.log(err);
+      })
+    },
+    userInfo: (req, res, next)=>{
+        const dbInstance = req.app.get('db')
+        const user_id = req.session.user_id
+        console.log(user_id, 'user_id')
+        dbInstance.get_user_info([user_id])
+        .then(user=>{
+            console.log('user', user)
+            res.status(200).send(user)
+        })
+        .catch((err) => {
+            res.status(500).send({ errorMessage: 'Something went wrong!' });
+            console.log(err);
+         })
+    }
+
+    
+}
